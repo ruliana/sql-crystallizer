4d3f9e57a2590332ddec4543decbd2a5a0d1081a
Ronie Uliana <ronie.uliana@shopify.com>
2019-12-09 12:53:07 -0500

    Refactor tests

--- spec/sql-crystallizer_spec.cr
+++ spec/sql-crystallizer_spec.cr
@@ -1,34 +1,31 @@
 require "./spec_helper"
 
 module Sql::Crystallizer
-  describe Sql::Crystallizer do
-    it "most basic select" do
-      ast = QueryParser.new.parse("select 1")
+  class Assert
+    def self.transform(source, target)
+      ast = QueryParser.new.parse(source)
       visitor = PlainStringVisitor.new
       visitor.visit(ast)
-      visitor.rslt.to_str.should eq "select 1"
+      visitor.rslt.to_str.should eq target
+    end
+  end
+
+  describe Sql::Crystallizer do
+    it "most basic select" do
+      Assert.transform("select 1", "select 1")
     end
 
     describe "aliases" do
       it "with no separator" do
-        ast = QueryParser.new.parse("select 1 test")
-        visitor = PlainStringVisitor.new
-        visitor.visit(ast)
-        visitor.rslt.to_str.should eq "select 1 as test"
+        Assert.transform("select 1 test", "select 1 as test")
       end
 
       it "with AS" do
-        ast = QueryParser.new.parse("select 1 as test")
-        visitor = PlainStringVisitor.new
-        visitor.visit(ast)
-        visitor.rslt.to_str.should eq "select 1 as test"
+        Assert.transform("select 1 as test", "select 1 as test")
       end
 
       it "with (=) equal" do
-        ast = QueryParser.new.parse("select test = 1")
-        visitor = PlainStringVisitor.new
-        visitor.visit(ast)
-        visitor.rslt.to_str.should eq "select 1 as test"
+        Assert.transform("select test = 1", "select 1 as test")
       end
     end
   end
