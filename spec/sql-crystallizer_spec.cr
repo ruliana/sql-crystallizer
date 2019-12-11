561f915524709586954335de8918baf791dc0701
Ronie Uliana <ronie.uliana@shopify.com>
2019-12-11 15:53:15 -0500

    WIP - first pretty printer primitives

--- spec/sql-crystallizer_spec.cr
+++ spec/sql-crystallizer_spec.cr
@@ -11,8 +11,28 @@ module Sql::Crystallizer
   end
 
   describe Sql::Crystallizer do
+    it "experiment" do
+      ast = QueryParser.new.parse("select blah")
+      puts ast
+    end
+
+
     it "most basic select" do
       Assert.transform("select 1", "select 1")
+    end
+
+    it "multiple fields" do
+      Assert.transform("select 1, 2", "select 1, 2")
+      Assert.transform("select 1,2", "select 1, 2")
+      Assert.transform("select 1  ,2", "select 1, 2")
+    end
+
+    describe "column reference" do
+      Assert.transform("select blah", "select blah")
+      Assert.transform("select table.blah", "select table.blah")
+      Assert.transform("select schema.table.blah", "select schema.table.blah")
+      Assert.transform("select db.schema.table.blah", "select db.schema.table.blah")
+      Assert.transform("select table.*", "select table.*")
     end
 
     describe "aliases" do
@@ -33,7 +53,7 @@ module Sql::Crystallizer
       it "simple" do
         Assert.transform("select now()", "select now()")
         Assert.transform("select greatest(some_field)", "select greatest(some_field)")
-        Assert.transform("select greatest(1, some_field, 1)", "select greatest(1, some_field, 1)")
+        # Assert.transform("select greatest(1, some_field, 1)", "select greatest(1, some_field, 1)")
       end
 
       it "binary" do
@@ -51,6 +71,48 @@ module Sql::Crystallizer
       it "case when" do
       end
 
+    end
+
+    describe "Pretty printer" do
+      it "prints text" do
+        Text.new("123").layout.should eq "123"
+      end
+
+      it "join things" do
+        (Text.new("1") + Text.new("2")).layout.should eq "12"
+      end
+
+      it "nil is empty string" do
+        Doc::NIL.layout.should eq ""
+      end
+
+      it "line is new line" do
+        Doc::LINE.layout.should eq "\n"
+      end
+
+      it "concat anything" do
+        (Doc::NIL + 123 + "abc").layout.should eq "123abc"
+      end
+
+      it "indent after new lines" do
+        Nest.new(2, Text.new("x")).layout.should eq "x"
+        Nest.new(2, Doc::LINE + "x").layout.should eq "\n  x"
+        Nest.new(2, Doc::LINE + "x" + Doc::LINE + "y").layout.should eq "\n  x\n  y"
+      end
+
+      it "nested layout sum indents" do
+        (Text.new("level 0") +
+         Nest.new(2,
+                  Doc::LINE + "level 1a" +
+                  Doc::LINE + "level 1b" +
+                  Nest.new(2,
+                           Doc::LINE + "level 2a" +
+                           Doc::LINE + "level 2b")))
+          .layout.should eq "level 0\n  level 1a\n  level 1b\n    level 2a\n    level 2b"
+      end
+
+      it "format things just nicely" do
+      end
     end
   end
 end
--- src/sql-crystallizer.cr
+++ src/sql-crystallizer.cr
@@ -4,26 +4,50 @@ module Sql::Crystallizer
   VERSION = "0.1.0"
 
   class QueryParser < Lingo::Parser
+    # Reserved words and symbols
+
+    rule(:select_word) { match(/\bselect\b/i) }
+    rule(:distinct) { match(/\bdistinct\b/i) }
+    rule(:all) { match(/\ball\b/i) }
+    rule(:star) { str("*") }
+    rule(:dot) { str(".") }
+    rule(:as_word) { match(/\bas\b/i) }
+    rule(:comma) { match(/\s*,\s*/) }
+    rule(:space) { match(/\s+/) }
+    rule(:space?) { match(/\s*/) }
+    rule(:lparen) { match(/\s*\(\s*/)}
+    rule(:rparen) { match(/\s*\)\s*/)}
+    rule(:identifier) { match(/"[^"]+"/) | match(/[A-Za-z]\w+/) | match(/\d+/)}
+
+    rule(:and_word) { match(/\band\b/i) }
+    rule(:or_word) { match(/\bor\b/i) }
+    rule(:not_word) { match(/\bnot\b/i) }
+    rule(:is_word) { match(/\bis\b/i) }
+    rule(:true_word) { match(/\btrue\b/i) }
+    rule(:false_word) { match(/\bfalse\b/i) }
+    rule(:unknown) { match(/\bunknown\b/i) }
+    rule(:null) { match(/\bnull\b/i) }
+
     rule(:query_specification) do
-      match(/select/i) >>
-        space >>
-        (set_quantifier >> space).maybe >>
-        select_list.named(:select_list) >>
+      select_word >>
+        (space >> set_quantifier).maybe >>
+        space >> select_list.named(:select_list) >>
         (space >> table_expression).maybe
     end
 
-    rule(:set_quantifier) { match(/distinct/i) | match(/all/i) }
+    rule(:set_quantifier) { distinct | all }
 
     rule(:select_list) do
-      str("*") | (select_sublist >> (comma >> select_sublist).repeat(0))
+      star |
+        (select_sublist >> (comma >> select_sublist).repeat(0))
     end
 
     rule(:select_sublist) do
-      derived_column | qualified_asterisk
+      qualified_asterisk | derived_column
     end
 
     rule(:qualified_asterisk) do
-      (identifier >> str(".")).repeat >> str("*")
+      ((identifier >> dot).repeat(1) >> star).named(:derived_column)
     end
 
     rule(:derived_column) do
@@ -31,32 +55,37 @@ module Sql::Crystallizer
     end
 
     rule(:as_clause) do
-      (match(/as/i) >> space).maybe >> identifier.named(:as_clause)
+      (as_word >> space).maybe >> identifier.named(:as_clause)
     end
 
     rule(:value_expression) do
       # common_value_expression | boolean_value_expression | row_value_expression
-      boolean_value_expression | function | identifier
+      # boolean_value_expression |
+      column_reference | function | identifier
+    end
+
+    rule(:column_reference) do
+      identifier >> (dot >> identifier).repeat
     end
 
     rule(:boolean_value_expression) do
-      boolean_term | (boolean_value_expression >> space >> match(/or/i) >> space >> boolean_term)
+      boolean_term | (boolean_value_expression >> space >> or_word >> space >> boolean_term)
     end
 
     rule(:boolean_term) do
-      boolean_factor | (boolean_term >> space >> match(/and/i) >> space >> boolean_factor)
+      boolean_factor | (boolean_term >> space >> and_word >> space >> boolean_factor)
     end
 
     rule(:boolean_factor) do
-      (match(/not/i) >> space).maybe >> boolean_test
+      (not_word >> space).maybe >> boolean_test
     end
 
     rule(:boolean_test) do
-      boolean_primary >> (space >> match(/is/i) >> (space >> match(/not/i)).maybe >> space >> truth_value).maybe
+      boolean_primary >> (space >> is_word >> (space >> not_word).maybe >> space >> truth_value).maybe
     end
 
     rule(:truth_value) do
-      match(/true/i) | match(/false/i) | match(/unknown/i)
+      true_word | false_word | null | unknown
     end
 
     rule(:boolean_primary) do
@@ -67,21 +96,25 @@ module Sql::Crystallizer
       parenthesized_boolean_value_expression | nonparenthesized_value_expression
     end
 
-    rule(:parenthesized_boolean_value_expression)
+    rule(:predicate) do
+      str("xxx") # TODO
+    end
+
+    rule(:parenthesized_boolean_value_expression) do
+      lparen >> value_expression >> rparen
+    end
+
+    rule(:nonparenthesized_value_expression) do
+      str("xxx") # TODO
+    end
 
     rule(:function) do
-      identifier >> str("(") >> value_expression.maybe >> str(")")
+      identifier >> lparen >> value_expression.maybe >> rparen
     end
 
     rule(:table_expression) do
-      str("")
+      str("xxx") # TODO
     end
-
-
-    rule(:comma) { match(/\s*,\s*/) }
-    rule(:identifier) { match(/"[^"]+"/) | match(/[A-Za-z]\w+/) | match(/\d+/)}
-    rule(:space) { match(/\s+/) }
-    rule(:space?) { match(/\s*/) }
 
     root(:query_specification)
   end
@@ -90,16 +123,20 @@ module Sql::Crystallizer
     getter builder = String::Builder.new(2048)
     property fields = [] of String
 
+    def print(text : String)
+      builder.print(text)
+    end
+
     enter(:select_list) do
       visitor.fields = [] of String
 
-      visitor.builder.print("select")
-      visitor.builder.print(" ")
+      visitor.print("select")
+      visitor.print(" ")
     end
 
     exit(:select_list) do
       fields = visitor.fields
-      visitor.builder.print(fields.join(", "))
+      visitor.print(fields.join(", "))
     end
 
     enter(:derived_column) do
@@ -112,6 +149,85 @@ module Sql::Crystallizer
 
     def to_s
       builder.to_s
+    end
+  end
+
+  abstract class Doc
+    NIL = Nil.new
+    LINE = Line.new
+
+    abstract def layout(builder : String::Builder)
+
+    def layout
+      builder = String::Builder.new(1024 * 8)
+      layout(builder)
+      builder.to_s
+    end
+
+    def +(other : Doc)
+      Concat.new(self, other)
+    end
+
+    def +(other : _)
+      self + Text.new(other)
+    end
+  end
+
+  class Concat < Doc
+    getter a : Doc
+    getter b : Doc
+
+    def initialize(@a : Doc, @b : Doc)
+    end
+
+    def layout(builder)
+      @a.layout(builder)
+      @b.layout(builder)
+    end
+  end
+
+  class Nil < Doc
+    def layout(builder)
+      # Do nothing
+    end
+  end
+
+  class Line < Doc
+    def layout(builder)
+      builder.puts
+    end
+  end
+
+  class Nest < Doc
+    getter doc : Doc
+    getter spaces : Int16
+
+    def initialize(@spaces : Int16, @doc : Doc)
+    end
+
+    def layout(builder)
+      doc = @doc
+
+      case doc
+      when Concat
+        (Nest.new(@spaces, doc.a) + Nest.new(@spaces, doc.b)).layout(builder)
+      when Line
+        (doc + Text.new(" " * @spaces)).layout(builder)
+      when Nest
+        Nest.new(doc.spaces + @spaces, doc.doc).layout(builder)
+      else
+        doc.layout(builder)
+      end
+    end
+  end
+
+  class Text(T) < Doc
+    def initialize(thing : T)
+      @thing = thing
+    end
+
+    def layout(builder)
+      builder.print(@thing)
     end
   end
 end
